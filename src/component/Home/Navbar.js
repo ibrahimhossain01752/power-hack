cafbb246e81366774ebc49c982717a6ea24f0481
ibrahimhossain01752 <ibrahimhossain01752@gmail.com>
2023-01-29 19:01:00 +0600

    nav addded

---
+++ src/component/Home/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+           <nav class="navbar navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand mx-5">Navbar</a>
+    <div class="d-flex mx-5">
+     <h6>Paid Total</h6>
+ </div>
+  </div>
+</nav>
+        </div>
+    );
+};
+
+export default Navbar;
