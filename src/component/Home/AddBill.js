3b6e010c65b74e890ea2b8bec094cc08d74c9efc
ibrahimhossain01752 <ibrahimhossain01752@gmail.com>
2023-01-29 19:20:34 +0600

    AddBill

--- src/component/Home/AddBill.js
+++ src/component/Home/AddBill.js
@@ -2,8 +2,19 @@ import React from 'react';
 
 const AddBill = () => {
     return (
-        <div>
-            
+        <div class="container mt-5">
+            <nav class="navbar navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand">Navbar</a>
+    <form>
+    <input class="form-control  " type="search" placeholder="Search" aria-label="Search"/>
+    </form>
+    <form class="d-flex">
+     
+      <button class="btn btn-outline-success" type="submit">Add New</button>
+    </form>
+  </div>
+</nav>
         </div>
     );
 };
